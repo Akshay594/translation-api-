0eb03873f401dd8a973cb0c29fcb0f7991405a27
mineralsmighty <141164640+mineralsmighty@users.noreply.github.com>
2023-11-28 05:31:29 +0530

    added the Swagger UI

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-from fastapi import FastAPI, HTTPException
-from starlette.middleware.cors import CORSMiddleware as CORSMiddleware  # noqa
-from googletrans import Translator
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_restx import Api, Resource, fields
 import unicodedata
-from pydantic import BaseModel
-from mangum import Mangum
+from googletrans import Translator
 
-app = FastAPI()
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+api = Api(app, version='1.0', title='Translation API',
+          description='A simple Translation API')
 translator = Translator()
-handler = Mangum(app)
 
-# Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-)
+ns = api.namespace('translate', description='Translation operations')
 
-class TranslationInput(BaseModel):
-    input_text: str
+translation_model = api.model('TranslationInput', {
+    'input_text': fields.String(required=True, description='Text to translate'),
+    'dest': fields.String(description='Destination language (default: en)')
+})
 
 def remove_diacritics(text):
     if text is None:
@@ -24,24 +23,32 @@ def remove_diacritics(text):
     normalized_text = unicodedata.normalize('NFD', text)
     return ''.join(char for char in normalized_text if unicodedata.category(char) != 'Mn')
 
-@app.post("/translate/")
-async def translate_text(input_data: TranslationInput, dest: str = 'en'):
-    input_text = input_data.input_text
-    try:
-        translated_text = translator.translate(input_text, dest=dest).text
-        detected_lang = translator.detect(input_text).lang
-        print(translator.translate(input_text, dest=dest).pronunciation)
-        
-        pronunciation = translator.translate(input_text, dest=dest).pronunciation
-        pronunciation = remove_diacritics(pronunciation)
+@ns.route('/')
+class Translate(Resource):
+    @ns.doc('translate_text')
+    @ns.expect(translation_model, validate=True)
+    def post(self):
+        input_data = request.json
+        input_text = input_data['input_text']
+        dest = input_data.get('dest', 'en')
 
-        print("detected_lang: ", detected_lang)
-        print("translated_text: ", translated_text)
-        return {"translated_text": translated_text, "pronunciation": pronunciation, "detected_language": detected_lang}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        try:
+            translated_text = translator.translate(input_text, dest=dest).text
+            detected_lang = translator.detect(input_text).lang
+            pronunciation = translator.translate(input_text, dest=dest).pronunciation
+            pronunciation = remove_diacritics(pronunciation)
 
+            return {
+                "translated_text": translated_text, 
+                "pronunciation": pronunciation, 
+                "detected_language": detected_lang
+            }
+        except Exception as e:
+            return {"error": str(e)}, 500
+
+@app.route("/")
+def hello_world():
+    return "Hello, World! This is a simple Flask API for translation."
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    app.run(host="0.0.0.0", port=8000)
